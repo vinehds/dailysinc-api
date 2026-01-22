68e3a452dfdc200b6e547689dc543271abbdd0c5
Vinicius Dalaqua <viniciushdalaqua@gmail.com>
2026-01-21 23:38:25 -0300

    first commit

---
+++ src/main/java/com/vinehds/dailysinc/DailysincApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinehds.dailysinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailysincApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailysincApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinehds/dailysinc/DailysincApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinehds.dailysinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailysincApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
